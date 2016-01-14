e4dba69a63fba4c493b1df7aee4897800b869511
ALagunas <amadorlagunas98@gmail.com>
2016-01-14 09:24:17 -0800

    Changed the names of the classes

--- app/src/main/java/com/example/csastudent2015/fitnessapp/StrengthTrainingWorkout.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/StrengthTrainingWorkout.java
@@ -3,7 +3,7 @@ package com.example.csastudent2015.fitnessapp;
 /**
  * Created by csastudent2015 on 1/14/16.
  */
-public class StrengthTraining extends Workout{
+public class StrengthTrainingWorkout extends Workout{
     protected double weight;
     protected int reps;
 
