a6fc803720f46e241daa57824915fed26973d08a
Koda98 <1624udon@gmail.com>
2016-01-14 09:19:39 -0800

    added variables

--- app/src/main/java/com/example/csastudent2015/fitnessapp/StrengthTraining.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/StrengthTraining.java
@@ -4,6 +4,7 @@ package com.example.csastudent2015.fitnessapp;
  * Created by csastudent2015 on 1/14/16.
  */
 public class StrengthTraining extends Workout{
-
+    protected double weight;
+    protected int reps;
 
 }
