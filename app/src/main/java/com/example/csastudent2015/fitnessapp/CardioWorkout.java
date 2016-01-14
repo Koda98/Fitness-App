3cbaf90bd28e6ddceeab8b854562290d8c53258f
kriskriskris10 <kristenk210@gmail.com>
2016-01-14 09:15:23 -0800

    HA

--- app/src/main/java/com/example/csastudent2015/fitnessapp/CardioWorkout.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/CardioWorkout.java
@@ -1,8 +1,12 @@
 package com.example.csastudent2015.fitnessapp;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by csastudent2015 on 1/14/16.
  */
 public class CardioWorkout extends Workout{
-
+    protected TimeUnit timer;
+    protected Double distance;
+    protected String effort;
 }
