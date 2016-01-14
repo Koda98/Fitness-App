a23f3911c202107de10516477cc0acab07d1e732
kriskriskris10 <kristenk210@gmail.com>
2016-01-14 09:26:10 -0800

    HA

--- app/src/main/java/com/example/csastudent2015/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/MainActivity.java
@@ -7,13 +7,12 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-//a test comment
+//a test commen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
