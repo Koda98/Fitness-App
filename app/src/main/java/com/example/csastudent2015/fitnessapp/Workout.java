865daca1e81c664a0c4933a6ad5b63c8557552e0
ALagunas <amadorlagunas98@gmail.com>
2016-01-14 09:06:30 -0800

    I added the Workout.java class

---
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/Workout.java
@@ -0,0 +1,12 @@
+package com.example.csastudent2015.fitnessapp;
+
+import java.util.Date;
+
+/**
+ * Created by csastudent2015 on 1/14/16.
+ */
+public class Workout {
+    protected String name;
+    protected Date date;
+    protected Boolean completed;
+}
