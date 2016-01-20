2c22368b0c4df418133cdd1ebeb58133a3f3fd30
Koda98 <1624udon@gmail.com>
2016-01-20 08:57:11 -0800

    asdf

--- app/src/main/java/com/example/csastudent2015/fitnessapp/NewWorkoutFragment.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/NewWorkoutFragment.java
@@ -1,5 +1,6 @@
 package com.example.csastudent2015.fitnessapp;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,17 +10,19 @@ import android.widget.Button;
 /**
  * Created by csastudent2015 on 1/15/16.
  */
-public class NewWorkoutFragment {
+public class NewWorkoutFragment extends Fragment{
     private Button mSportButton;
     private Button mSTButton;
     private Button mCardioButton;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_new_workout, container, false);
 
-        mCardioButton = (Button)rootView.findViewById(R.id.cardio_button);
-        mSportButton = (Button)rootView.findViewById(R.id.sport_button);
-        mSTButton = (Button)rootView.findViewById(R.id.st_button);
+        mCardioButton = (Button) rootView.findViewById(R.id.cardio_button);
+        mSportButton = (Button) rootView.findViewById(R.id.sport_button);
+        mSTButton = (Button) rootView.findViewById(R.id.st_button);
+
+        return rootView;
     }
 }
