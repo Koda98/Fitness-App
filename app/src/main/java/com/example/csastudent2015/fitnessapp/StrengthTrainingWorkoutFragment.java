4539d16a998d52e3f0c7d5675695abf8b15c67e0
csastudent2015 <kriskriskris10>
2016-01-20 08:59:51 -0800

    vsdgerfgsfd

--- app/src/main/java/com/example/csastudent2015/fitnessapp/StrengthTrainingWorkoutFragment.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/StrengthTrainingWorkoutFragment.java
@@ -1,9 +1,19 @@
 package com.example.csastudent2015.fitnessapp;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by csastudent2015 on 1/20/16.
  */
 public class StrengthTrainingWorkoutFragment extends Fragment {
+
+  
+    protected EditText mEnterFirstName;
+    protected EditText mEnterLastName;
+
+    private Context mAppContext;
+
 }
