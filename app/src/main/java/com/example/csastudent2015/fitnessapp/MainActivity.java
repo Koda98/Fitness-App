7efd0db5073731f32ef3c08b83bba5f63bb6bc8d
kriskriskris10 <kristenk210@gmail.com>
2016-01-26 08:34:06 -0800

    LOOOOOOOL

--- app/src/main/java/com/example/csastudent2015/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/MainActivity.java
@@ -28,7 +28,6 @@ public String value = "poo";
             }
         });
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/example/csastudent2015/fitnessapp/NewWorkoutActivity.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/NewWorkoutActivity.java
@@ -2,11 +2,15 @@ package com.example.csastudent2015.fitnessapp;
 
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by csastudent2015 on 1/20/16.
@@ -19,15 +23,24 @@ public class NewWorkoutActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_workout);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragementContainer);
+        fragment = new NewWorkoutFragment();
+        fm.beginTransaction()
+                .add(R.id.fragementContainer, fragment)
+                .commit();
 
-        NewWorkoutFragment fragment = new NewWorkoutFragment();
-        fragmentTransaction.add(R.id.new_workout, fragment,TAG);
-        fragmentTransaction.commit();
     }
 
     //todo: make oncreateView and put fragment tranasction stuff in it
+
+
+    /*@Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment_new_workout, container, false);
+        return rootView;
+    }
+*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
