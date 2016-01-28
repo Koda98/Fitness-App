df36c29f9b9b5be351033ec3be2637de6a4f868e
csastudent2015 <kriskriskris10>
2016-01-28 09:37:30 -0800

    vsdgerfgsfd

--- app/src/main/java/com/example/csastudent2015/fitnessapp/NewWorkoutActivity.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/NewWorkoutActivity.java
@@ -1,22 +1,57 @@
 package com.example.csastudent2015.fitnessapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by csastudent2015 on 1/20/16.
  */
 public class NewWorkoutActivity extends FragmentActivity {
     public static final String TAG = "NewWorkoutActivity";
+
+    private Button SportButton;
+    private Button CardioButton;
+    private Button STButton;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_user, container, false);
+
+        getActivity().setTitle("User");
+
+
+
+
+        SportButton = (Button)rootView.findViewById(R.id.sport_button);
+        CardioButton = (Button)rootView.findViewById(R.id.cardio_button);
+        STButton = (Button)rootView.findViewById(R.id.st_button);
+
+
+        SportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e()
+            }
+        });
+
+        //LOG THIS STUFF AND MAKE ONCLICKLISTENERS FOR ALL BUTTONS AND INFLATE FRAGS WHEN BUTTON PUSHED
+
+        return rootView;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
